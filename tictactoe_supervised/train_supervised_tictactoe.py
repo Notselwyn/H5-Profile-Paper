a2a545dde52a0e4b01f8f5139b57b72275981cc1
notselwyn <laurencewit4@gmail.com>
2021-11-05 15:36:04 +0100

    Optimized the testing part of the supervised training script

--- tictactoe_supervised/train_supervised_tictactoe.py
+++ tictactoe_supervised/train_supervised_tictactoe.py
@@ -1,4 +1,3 @@
-#from sklearn.model_selection import train_test_split
 from sklearn import tree
 import pandas as pd
 import pickle
@@ -6,24 +5,11 @@ import graphviz
 import re
 from itertools import product
 
-
 dataframe = pd.read_csv("../tictactoe_dataset.csv", dtype=str)
-
-# don't split dataset: doing manual checking so train / test split is unimportant
-#train, test = train_test_split(dataframe, test_size=0.03, random_state=3123)
 model = tree.DecisionTreeClassifier(max_depth=20)
 model.fit(tuple(tuple(int(y) for y in x) for x in dataframe['state']), tuple(int(x) for x in dataframe['best']))
-
-succeeded = 0
-p = 0
-prod_len = 3**9
-old_frac = 0
-for i in product(range(3), repeat=9):
-    if int(model.predict([i])[0]) == int(dataframe.loc[dataframe['state'] == "".join(map(str, i))]['best']):
-        succeeded += 1
-    p += 1
-    if old_frac != (old_frac := p / prod_len):
-        print(f"success rate: {round(succeeded / p * 100, 1)}%; progression: {round(old_frac * 100, 1)}%" + " " * 6, end="\r")
+best_list = tuple(dataframe['best'].astype(int).to_list())
+print(f"accuracy: {round(sum(map(lambda x: x[1] == best_list[x[0]], enumerate(model.predict([*product(range(3), repeat=9)])))) / 196.83, 1)}%")
 
 with open("supervised_tictactoe.pkl", "wb") as f:
     pickle.dump(model, f)
