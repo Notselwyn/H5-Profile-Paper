c9cf26a388f992e8d4a60928978c686eb82e7cf0
notselwyn <laurencewit4@gmail.com>
2021-11-19 12:19:21 +0100

    Added play_supervised_tictactoe.py

--- tictactoe_supervised/play_supervised_tictactoe.py
+++ tictactoe_supervised/play_supervised_tictactoe.py
@@ -1,39 +1,31 @@
 import pickle
-import zmq
 
 
 class Game:
-    def __init__(self, board: list=[0]*9, model=None, lport=None, sport=None):
+    def __init__(self, board: list=[0]*9, model=None, dataset=None, gather_stats=False):
         self.original_board = board
         self.board = board
         self.model = model
-        self.rpc_lport = lport
-        self.rpc_sport = sport
+        self.dataset = dataset
         self.ask_index = 0
         self.last_cell = -1
+        self.gather_stats = gather_stats
+        self.print = print
+
+        if self.gather_stats:
+            self.print = lambda *arg, end=None: None
 
         if model is None:
-            if lport is None:
+            if dataset is None:
                 self.ask_array = [self.ask_player_cell, self.ask_player_cell]
             else:
-                self.ask_array = [self.ask_player_cell, self.ask_rpc_cell()]
+                self.ask_array = [self.ask_player_cell, self.ask_dataset_cell]
         else:
-            if lport is None:
+            if dataset is None:
                 self.ask_array = [self.ask_player_cell, self.ask_model_cell]
             else:
-                self.ask_array = [self.ask_model_cell, self.ask_rpc_cell]
-
-        if self.rpc_lport is not None:
-            self.rpc_scontext = zmq.Context()
-            self.rpc_ssocket = self.rpc_scontext.socket(zmq.PUB)
-            self.rpc_ssocket.connect(f"tcp://localhost:{sport}")
-
-            self.rpc_lcontext = zmq.Context()
-            self.rpc_lsocket = self.rpc_lcontext.socket(zmq.SUB)
-            self.rpc_lsocket.bind(f"tcp://localhost:{lport}")
-
-            print("sent pakket:", self.rpc_ssocket.send(b"INI"))
-            print("touched down confirmed:", self.rpc_lsocket.recv())
+                self.ask_array = [self.ask_model_cell, self.ask_dataset_cell]
+                #self.ask_array = [self.ask_dataset_cell, self.ask_model_cell]
 
     def __repr__(self):
         st = ""
@@ -72,73 +64,99 @@ class Game:
                 print(f"Player {i+1} is a human")
             elif self.ask_array[i] == self.ask_model_cell:
                 print(f"Player {i+1} is a ML model")
-            elif self.ask_array[i] == self.ask_rpc_cell:
-                print(f"Player {i+1} is a RPC server")
+            elif self.ask_array[i] == self.ask_dataset_cell:
+                print(f"Player {i+1} is a dataset")
 
     def ask_player_cell(self):
         cell = int(input("Choose a cell (1-9): "))-1
         while cell < 0 or cell > 8 or self.board[cell] != 0:
-            print("Invalid cell!")
+            self.print("Invalid cell!")
             cell = int(input("Choose a cell (1-9): "))-1
         self.last_cell = cell
         return self.last_cell
 
     def ask_model_cell(self):
-        if self.model is not None:
+        cell = self.model.predict([self.board])[0]
+        if cell < 0:
+            return cell
+        if self.board[cell] != 0:
+            self.print(f"The ML made a mistake by picking invalid cell {cell+1}")
             cell = self.model.predict([self.board])[0]
-            if self.board[cell] != 0:
-                print(f"The ML made a mistake by picking invalid cell {cell+1}")
-                cell = self.model.predict([self.board])[0]
-            print(f"The ML model picked cell {cell+1}")
-        else:
-            cell = int(input("Choose a cell (1-9): "))-1
+        self.print(f"The ML model picked cell {cell+1}")
         self.last_cell = cell
         return self.last_cell
 
-    def ask_rpc_cell(self):
-        self.rpc_socket.send(f"REQ,{self.last_cell}")
-        self.last_cell = int(self.rpc_socket.recv())
-        return self.last_cell
+    def ask_dataset_cell(self):
+        s = 0
+        for p, x in enumerate(self.board):
+            s += x * (3 ** p)+1
+        with open(self.dataset) as f:
+            best = int(f.readlines()[s].split(",")[1])
+        self.print(f"The dataset picked cell {best+1}")
+        self.last_cell = best
+        return best
 
     def play(self):
-        self.print_players()
+        if not self.gather_stats:
+            self.print_players()
+        if self.gather_stats:
+            self.board = [2 if x == 1 else 1 if x == 2 else x for x in self.board]
         while True:
-            print(self.__repr__())
-            self.board[self.ask_array[self.ask_index]()] = 1
-            self.ask_index = (self.ask_index + 1) % 2
-            if (winner := self.get_winner()) != 0:
+            self.print(self.__repr__())
+            ncell = self.ask_array[self.ask_index]()
+            if ncell < 0:
+                self.print(f"Player {self.ask_index+1} capitalized because it thought the game would end in ", end="")
+                if ncell == -2:
+                    self.print("a win")
+                elif ncell == -3:
+                    self.print("a loss")
+                elif ncell == -4:
+                    self.print("a draw")
+                else:
+                    self.print("an invalid state")
+                winner = ncell
                 break
-            print(self.__repr__())
-            self.board[self.ask_array[self.ask_index]()] = 2
+
+            self.board[ncell] = self.ask_index+1
             self.ask_index = (self.ask_index + 1) % 2
             if (winner := self.get_winner()) != 0:
                 break
 
-        print(self.__repr__())
-        if self.rpc_port is not None:
-            self.rpc_socket.send("RST")
-        if winner == 3:
-            print("It's a draw!")
-        else:
-            print(f"Player {winner} won!")
+        if not self.gather_stats:
+            self.print(self.__repr__())
+            if winner == 3:
+                self.print("It's a draw!")
+            elif winner != 0:
+                self.print(f"Player {winner} won!")
 
-        if "n" not in input("Play again? [Y/n]: ").lower():
+        if self.gather_stats:
+            return winner
+        elif "n" not in input("Play again? [Y/n]: ").lower():
             self.reset()
             self.shuffle_ask_array()
             self.play()
-        else:
-            self.rpc_socket.send("EXT")
 
+    def stats(self):
+        with open(self.dataset) as f:
+            lines = f.readlines()[1:]
+
+        results = {}
+        i = 0
+        self.print_players()
+        for l in lines:
+            self.board = [int(x) for x in l.split(",")[0]]
+            if self.get_winner() == 0:
+                r = self.play()
+                if r not in results:
+                    results[r] = 0
+                results[r] += 1
+                if i % 50 == 0:
+                    print(results)
+                i += 1
 
 
 with open("supervised_tictactoe.pkl", "rb") as f:
     tictactoe_ml_model = pickle.load(f)
 
-rpc_sport = None
-rpc_lport = None
-#if "y" in input("Do you want to run RPC for the C++ AI? [y/N]: ").lower():
-#    rpc_sport = int(input("C++ RPC port (1-65535): "))
-#    rpc_lport = int(input("Own listen port (1-65535): "))
-rpc_sport = 12345
-rpc_lport = 12346
-Game(model=tictactoe_ml_model, lport=rpc_lport, sport=rpc_sport).play()
+Game(model=tictactoe_ml_model, dataset="../tictactoe_dataset.csv", gather_stats=True).stats()
+#Game(model=tictactoe_ml_model).play()
